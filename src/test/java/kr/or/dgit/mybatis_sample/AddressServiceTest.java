89495b9670e0e9e5cccd3df9ec2ed45cbf92bdee
HyeSeon Yang <skcm19@naver.com>
2018-02-14 11:21:55 +0900

    매퍼 XML row바운드 sqlLimit추가
    select addr_id, street, city, state, zip, country
    from addresses
    limit 3,3;

--- src/test/java/kr/or/dgit/mybatis_sample/AddressServiceTest.java
+++ src/test/java/kr/or/dgit/mybatis_sample/AddressServiceTest.java
@@ -1,8 +1,8 @@
 package kr.or.dgit.mybatis_sample;
 
-import static org.junit.Assert.fail;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.RowBounds;
 import org.junit.AfterClass;
@@ -27,10 +27,38 @@ public class AddressServiceTest {
 	}
 
 	@Test
-	public void test() {
+	public void test1SelectAddressByAll() {
 		RowBounds rowBounds = new RowBounds(0,3);
 		List<Address> lists = service.findAddressByAll(rowBounds);
 		Assert.assertNotNull(lists);
 	}
+	
+	@Test
+	public void test2SelectAddressByAll() {
+		RowBounds rowBounds = new RowBounds(2, 3);
+        List<Address> lists = service.findAddressByAll(rowBounds);
+        System.out.println(lists.size());
+        Assert.assertNotNull(lists);
+	}
 
+	@Test
+	public void test3SelectAddressLimitByAll() {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("offset", 0);
+		map.put("limit", 3);
+        List<Address> lists = service.selectAddressLimitByAll(map);
+        System.out.println(lists.size());
+        Assert.assertNotNull(lists);
+
+	}
+	
+	@Test
+	public void test4SelectAddressLimitByAll() {
+		Map<String, Integer> map = new HashMap<>();
+		map.put("offset", 3);
+		map.put("limit", 3);
+        List<Address> lists = service.selectAddressLimitByAll(map);
+        System.out.println(lists.size());
+        Assert.assertNotNull(lists);
+	}
 }
